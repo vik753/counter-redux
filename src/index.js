68edc8c9db0296677118a81129a0c123c5333848
Igor Korenets <vik753@gmail.com>
2020-05-12 15:23:10 +0300

    added redux-devtool-extension

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+/*
+https://github.com/zalmoxisus/redux-devtools-extension#usage
+Use redux-devtools-extension package from npm
+To make things easier, there's an npm package to install:
+
+npm install --save redux-devtools-extension
+
+and to use like so:
+
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+));
+* */
+
+const logger = (store) => (next) => (action) => {
+  const result = next(action);
+  console.log("Logger-Action: ", action);
+  console.log("Logger-Store: ", store.getState());
+  return result;
+};
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
