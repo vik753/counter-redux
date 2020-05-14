22454a537a4888e206d72ce11dbe058e424d9f11
Igor Korenets <vik753@gmail.com>
2020-05-14 14:01:59 +0300

    made build

---
+++ build/precache-manifest.e97ec0284a240035a895a16f8a373811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2d751184697515445b94a1f2db0f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c736da708aa5d923be7",
+    "url": "/static/css/main.d195b89e.chunk.css"
+  },
+  {
+    "revision": "b54dbe24c91c6144096f",
+    "url": "/static/js/2.9f5b32b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f5b32b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c736da708aa5d923be7",
+    "url": "/static/js/main.99af5632.chunk.js"
+  },
+  {
+    "revision": "f37443532fd28689745b",
+    "url": "/static/js/runtime-main.68dcdfd9.js"
+  }
+]);
